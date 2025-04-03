6f7f3da4af0b51c15f86f87c67bb073166e26386
kiyameh <kiyameh@outlook.com>
2025-04-03 21:03:03 +0200

    Primera demo de react-resume-kit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import styles from './App.module.css'
+import {
+  ResumeLayout,
+  ResumeHeader,
+  ResumeAbout,
+  ResumeContact,
+  ResumeCourses,
+  ResumeProjects,
+  ResumeSoftSkills,
+  ResumeTechnologies,
+  ResumeWorks,
+} from 'react-resume-kit'
+import 'react-resume-kit/styles'
+
+import {content} from './data/content'
+
+function App() {
+  return (
+    <div>
+      <div className={styles.frame}>
+        <ResumeLayout
+          resumeContent={content}
+          enableLanguageSwitch
+          enablePdfDownload
+          initialLanguage="es"
+        >
+          <ResumeHeader />
+          <ResumeAbout />
+          <ResumeWorks />
+          <ResumeCourses />
+          <ResumeTechnologies />
+          <ResumeSoftSkills />
+          <ResumeProjects />
+          <ResumeContact />
+        </ResumeLayout>
+      </div>
+      <div className={styles.background} />
+    </div>
+  )
+}
+
+export default App
